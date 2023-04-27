84df280f8b48eb663c8716b62b2cc10b0e3cfe52
Emma Ericsson <emma@krlsg.se>
2023-04-27 11:25:41 +0200

    ShitOS support

---
+++ gcc/config/shitos.h
@@ -0,0 +1,14 @@
+#undef TARGET_SHITOS
+#define TARGET_SHITOS 1
+
+#undef LIB_SPEC
+#define LIB_SPEC "-lc"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+#undef STANDARD_STARTFILE_PREFIX
+#define STANDARD_STARTFILE_PREFIX "/lib/"
